aca993db14833c84c98a78ab333016813d8d62ee
ggyu <kcpooh0202@naver.com>
2021-03-11 17:22:37 +0900

    board

---
+++ models/user.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    "User",
+    {
+      email: {
+        type: DataTypes.STRING(30),
+        allowNull: true,
+        unique: true,
+      },
+      nickname: {
+        type: DataTypes.STRING(20),
+        allowNull: true,
+      },
+      password: {
+        type: DataTypes.STRING(100),
+        allowNull: true,
+      },
+    },
+    {
+      charset: "utf8",
+      collate: "utf8_general_ci",
+    }
+  );
+  User.associate = (db) => {};
+  return User;
+};
